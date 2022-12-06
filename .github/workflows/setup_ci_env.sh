774e81830908e5e36d50834c78297a3e16e3512a
Hécate Moonlight <hecate+github@glitchbra.in>
2022-12-06 18:43:02 +0100

    Fix CI

--- .github/workflows/setup_ci_env.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-echo "Setting up the environment for linux"
-sudo apt-get update
-sudo apt install -y libgirepository1.0-dev libwebkit2gtk-4.0-dev libgtksourceview-3.0-dev
