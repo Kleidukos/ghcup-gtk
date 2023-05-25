239c017587bd56d6e0bdf54256716f8ff89b1004
Brad Neimann <brad.neimann@hotmail.com>
2023-05-25 16:37:33 +1000

    Fourmolu formatting

--- src/UI/Install.hs
+++ src/UI/Install.hs
@@ -11,19 +11,21 @@ mockInstall selfSwitch toastOverlay app state toolDescription = do
   curState <- selfSwitch.getState
   -- don't handle case when state is being reset: e.g. when 'Cancel'
   -- button is pressed on message box
-  (True<$) $ when (curState /= state) $ do
+  (True <$) $ when (curState /= state) $ do
     let stateText = if state then "install" else "uninstall"
         stateTextUpper = if state then "Install" else "Uninstall"
         messageText =
-          "Are you sure you want to " <>
-          stateText <> " " <>
-          toolDescription <> "?"
+          "Are you sure you want to "
+            <> stateText
+            <> " "
+            <> toolDescription
+            <> "?"
 
     messageDialog <-
       Adw.messageDialogNew
-      (Just app)
-      (Just $ "Confirm " <> stateText)
-      (Just messageText)
+        (Just app)
+        (Just $ "Confirm " <> stateText)
+        (Just messageText)
     messageDialog.addResponse "cancel" "Cancel"
     messageDialog.addResponse "doit" stateTextUpper
 
@@ -52,6 +54,5 @@ mockInstall selfSwitch toastOverlay app state toolDescription = do
                 ]
             Adw.toastOverlayAddToast toastOverlay notificationToast
             set selfSwitch [#state := state]
-      _ -> set selfSwitch [#active := not state]  -- reset position
-
+      _ -> set selfSwitch [#active := not state] -- reset position
     messageDialog.present
