2c6e7d2b5e3879dbfa6dc1debab35bcabeee86a9
Hécate Kleidukos <hecate+github@glitchbra.in>
2026-08-24 10:31:57 +0200

    Interface change switch added to preferences

--- src/gtk/UI/PreferencesWindow.hs
+++ src/gtk/UI/PreferencesWindow.hs
@@ -2,32 +2,57 @@ module UI.PreferencesWindow (present) where
 
 import Control.Monad (void)
 import Data.GI.Base
+import Data.Text (Text)
 import GI.Adw qualified as Adw
 
 import Config
 
 present :: Adw.ApplicationWindow -> Config -> (ConfigUpdate -> IO ()) -> IO ()
 present parent config onChanged = do
-  toggle <-
+  advancedToggle <-
+    new
+      Adw.SwitchRow
+      [ #title := "Advanced interface"
+      , #subtitle := "Sortable table with release dates and filters"
+      , #active := config.advancedInterface
+      ]
+
+  oldVersionsToggle <-
     new
       Adw.SwitchRow
       [ #title := "Older Versions"
-      , #subtitle := "List every version ghcup knows about"
+      , #subtitle := oldVersionsSubtitle config.advancedInterface
       , #active := config.showOldVersions
+      , #sensitive := not config.advancedInterface
       ]
-  void $ on toggle (PropertyNotify #active) $ \_ -> do
-    active <- toggle.getActive
+
+  void $ on advancedToggle (PropertyNotify #active) $ \_ -> do
+    active <- advancedToggle.getActive
+    set
+      oldVersionsToggle
+      [ #sensitive := not active
+      , #subtitle := oldVersionsSubtitle active
+      ]
+    onChanged (SetAdvancedInterface active)
+
+  void $ on oldVersionsToggle (PropertyNotify #active) $ \_ -> do
+    active <- oldVersionsToggle.getActive
     onChanged (SetShowOldVersions active)
 
-  group <- new Adw.PreferencesGroup [#title := "Display"]
-  group.add toggle
+  interfaceGroup <- new Adw.PreferencesGroup [#title := "Interface"]
+  interfaceGroup.add advancedToggle
+  displayGroup <- new Adw.PreferencesGroup [#title := "Display"]
+  displayGroup.add oldVersionsToggle
+
   page <- new Adw.PreferencesPage []
-  page.add group
-  window <-
-    new
-      Adw.PreferencesWindow
-      [ #transientFor := parent
-      , #modal := True
-      ]
+  page.add interfaceGroup
+  page.add displayGroup
+
+  window <- new Adw.PreferencesWindow [#transientFor := parent, #modal := True]
   window.add page
   window.present
+
+oldVersionsSubtitle :: Bool -> Text
+oldVersionsSubtitle advanced
+  | advanced = "Not used by the advanced interface"
+  | otherwise = "List every version ghcup knows about"
